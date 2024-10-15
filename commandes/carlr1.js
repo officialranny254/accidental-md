daed94a4c8a6b857926151a56e342d8bd210f3ad
MBUVI DAVID <147725809+cheekydavy@users.noreply.github.com>
2024-10-15 12:42:50 +0200

    Update carlr1.js

--- commandes/carlr1.js
+++ commandes/carlr1.js
@@ -3,10 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "speed", reaction: "🕳", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = '𝑯𝒆𝒍𝒍𝒐 𝒗𝒂𝒍𝒖𝒆𝒅 𝒖𝒔𝒆𝒓 𝒐𝒇 𝑪𝒚𝒃𝒆𝒓𝒊𝒐𝒏';
-    let d = '𝑻𝒉𝒆 𝒂𝒃𝒐𝒗𝒆 𝒎𝒆𝒔𝒔𝒂𝒈𝒆 𝒊𝒔 𝒕𝒐 𝒊𝒏𝒇𝒐𝒓𝒎 𝒚𝒐𝒖 𝒕𝒉𝒂𝒕 𝑪𝒚𝒃𝒆𝒓𝒊𝒐𝒏 𝒊𝒔 𝑪𝒖𝒓𝒓𝒆𝒏𝒕𝒍𝒚 𝒂𝒕 𝒂 𝒈𝒐𝒐𝒅 𝒔𝒑𝒆𝒆𝒅 𝒐𝒇 700.13/ms.';
+    let z = 'Hello user';
+    let d = 'Savage Md still active at a speed of 700.13/ms.';
     let varmess = z + d;
-    var img = 'https://telegra.ph/file/0e2976b99ebfc22a2753f.jpg';
+    var img = 'https://telegra.ph/file/a7ca8d4777fa8ad977795.jpg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
     //console.log("montest")
 });
